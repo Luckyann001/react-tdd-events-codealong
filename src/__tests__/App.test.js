a4248aa597dde2e4c91d8f23f90e7cd27beb8c26
Luckyann Kagendo <kagendoluckyann@gmail.com>
2025-10-29 16:46:41 +0300

    added lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,9 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+// app.test.js
+describe('Basic Test', () => {
+  test('should run successfully', () => {
+    expect(true).toBe(true);
+  });
+});
